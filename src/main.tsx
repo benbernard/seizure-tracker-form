3b38e2f2afe6feb31733905219f001c698484a3f
Ben Bernard <ben.bernard@instacart.com>
2025-02-08 23:06:29 -0800

    Some updates

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,18 @@ import { Amplify } from "aws-amplify";
 import { config } from "./amplifyconfiguration";
 import App from "./App.tsx";
 import "./index.css";
+import { generateClient } from "aws-amplify/data";
+import outputs from "../amplify_outputs.json";
+import type { Schema } from "../amplify/data/resource"; // Path to your backend resource definition
 
-Amplify.configure(config);
+Amplify.configure(outputs);
 
 const rootElement = document.getElementById("root");
 if (!rootElement) {
   throw new Error("Failed to find the root element");
 }
+
+const client = generateClient<Schema>();
 
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
